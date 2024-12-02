2a56d1a0e89303db1f1a3a3c9bc12ec53ef89022
Rys17 <ryslann95@mail.ru>
2024-12-02 07:42:14 +0300

    блок form

--- source/js/main.js
+++ source/js/main.js
@@ -3,8 +3,10 @@ import {playVideo} from './video.js';
 import {switchTab} from './tab-price.js';
 import {changeTab} from './accordeon-tab.js';
 import {changeAccordeon} from './accordeon.js';
+import { validateForm } from './form.js';
 
 switchTab();
 playVideo();
 changeTab();
 changeAccordeon();
+validateForm();
